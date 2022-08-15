6783fb6ed26c373c7d9a851d1abb4a58fd6c7cf2
Clayton0205 <claytonadonis5@gmail.com>
2022-08-15 12:02:22 +0200

    productspage

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,12 +1,20 @@
 <template>
     <div id="page">
         <div v-if="products">
-            <div v-for="product in products" :key="product">
-                <h1>{{ product.title }}</h1>
-                <p>{{ product.description }} AND {{ product.price }}</p>
+            <div class="container-fluid">
+                <div v-for="product in products" :key="product" id="box">
+                    <div class="row">
+                        <h3>{{ product.title }}</h3>
+                        <img :src="product.image" id="image">
+                        <br>
+                        <p>{{ product.description }}</p>
+                        <p>R{{ product.price }}</p>
+                    </div>
+
+                </div>
             </div>
         </div>
-        <div v-else>
+        <div v-else id="else">
             <h1>Mental Breakdown</h1>
         </div>
     </div>
@@ -37,8 +45,35 @@ export default {
 #page {
     background-color: #7d1b56;
     width: 100vw;
-    height: 100vh;
     overflow: hidden;
     width: 1519px;
+}
+
+.container-fluid {
+    display: flex;
+    flex-flow: row wrap;
+}
+
+
+#box {
+    background-color: white;
+    margin: auto;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    width: 500px;
+    height: 580px;
+    border: 1px solid black;
+    text-align: center;
+}
+
+#image {
+    margin: auto;
+    margin-bottom: 20px;
+    width: 400px;
+    height: 400px;
+}
+
+#else{
+    text-align: center;
 }
 </style>
